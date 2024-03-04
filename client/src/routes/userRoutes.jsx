641ad35d1ea496b6516d7da113839f4b8c70a3e2
Swarup Lamichhane <lswarup1234@gmail.com>
2024-03-04 13:04:34 +0545

    Added User and Admin routes

---
+++ client/src/routes/userRoutes.jsx
@@ -0,0 +1,24 @@
+const iconSize = 25;
+
+export default userRoutes = [
+  {
+    path: "/app/feed",
+    name: "Feed",
+    icon: "",
+  },
+  {
+    path: "/app/discover",
+    name: "Discover",
+    icon: "",
+  },
+  {
+    path: "/app/friends",
+    name: "Friends",
+    icon: "",
+  },
+  {
+    path: "/app/communities",
+    name: "Communities",
+    icon: "",
+  },
+];
