a92f43eacea8faec4b53c1ba48dbaf226b7f0666
Swarup Lamichhane <lswarup1234@gmail.com>
2024-03-13 15:41:10 +0545

    Update index.js with new imports

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,28 @@
 import Login from "./login/login";
-import Signup from "./signup/signup";
-import Dashboard from "./adminDashboard/dashboard";
 
-export { Login, Signup, Dashboard };
+import Signup from "./signup/signup";
+
+import Admin from "./adminDashboard/admin";
+import Dashboard from "./adminDashboard/components/dashboard";
+import AdminGames from "./adminDashboard/components/adminGames";
+import AdminNews from "./adminDashboard/components/adminNews";
+import Users from "./adminDashboard/components/users";
+
+import Error from "./error/error";
+
+import OTPInput from "./otpverification/otpInput";
+
+import ResetPassword from "./otpverification/resetPassword";
+
+export {
+  Login,
+  Signup,
+  Admin,
+  Error,
+  OTPInput,
+  ResetPassword,
+  Dashboard,
+  AdminGames,
+  AdminNews,
+  Users,
+};
