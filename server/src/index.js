882c2908589c683cd72b8a1f0717bc60df2193ae
Swarup Lamichhane <lswarup1234@gmail.com>
2024-03-13 15:42:39 +0545

    Add cookie-session and passport middleware

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,10 @@ const cors = require("cors");
 const connection = require("./database/connect.js");
 const fileUpload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+
+const passportSetup = require("./services/passport.js");
 
 const authRoute = require("./routes/auth.routes.js");
 const userRoute = require("./routes/user.routes.js");
@@ -18,6 +22,7 @@ const app = express();
 const corsOptions = {
   origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
   preflightContinue: false,
   optionSuccessStatus: 204,
 };
@@ -28,6 +33,15 @@ app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(fileUpload({ useTempFiles: true }));
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["cyberwolve"],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
