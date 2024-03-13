1362c48b3759480b05ef357e75cf8e878e11075b
Swarup Lamichhane <lswarup1234@gmail.com>
2024-03-13 15:40:50 +0545

    Update Tab component styling

--- client/src/components/navigation/Tab.jsx
+++ client/src/components/navigation/Tab.jsx
@@ -1,12 +1,12 @@
 const Tab = ({ title, onClick, isActive, icon }) => {
   return (
     <div
-      className={`flex justify-start ${
+      className={`flex md:w-[240px] justify-start ${
         isActive ? "text-primary bg-accent" : "text-secondary bg-primary"
       } font-medium p-3, text-base gap-5 px-4 cursor-pointer rounded-md w-full items-center py-3 ${
         !isActive && "hover:bg-gray-100"
       }`}
-      onClick={() => onClick(title)}
+      onClick={onClick}
     >
       <span>{icon}</span>
       {title}
