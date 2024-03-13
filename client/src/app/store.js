032100d4e90bb53d60fa46caaaace77e80d56b61
Swarup Lamichhane <lswarup1234@gmail.com>
2024-03-13 15:40:35 +0545

    Add modalReducer to store.js

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { authReducer, themeReducer } from "../features";
+import { authReducer, themeReducer, modalReducer } from "../features";
 
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
     user: authReducer,
+    modal: modalReducer,
   },
 });
