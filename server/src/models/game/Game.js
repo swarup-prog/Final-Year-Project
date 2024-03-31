0cca593beea288e67e1c85a1e792700717fcf2e4
Swarup Lamichhane <lswarup1234@gmail.com>
2024-03-31 13:32:43 +0545

    Refactor game type to genre in Game model

--- server/src/models/game/Game.js
+++ server/src/models/game/Game.js
@@ -9,8 +9,8 @@ const gameSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  type: {
-    type: String,
+  genre: {
+    type: [String],
     required: true,
   },
   description: {
