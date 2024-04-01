c039fa72894802f371999d9a998387a7d490dc79
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-01 11:19:40 +0545

    Update hover background color in Tab component

--- client/src/components/navigation/Tab.jsx
+++ client/src/components/navigation/Tab.jsx
@@ -4,7 +4,7 @@ const Tab = ({ title, onClick, isActive, icon }) => {
       className={`flex md:w-[240px] justify-start ${
         isActive ? "text-primary bg-accent" : "text-secondary bg-primary"
       } font-medium p-3, text-base gap-5 px-4 cursor-pointer rounded-md w-full items-center py-3 ${
-        !isActive && "hover:bg-gray-100"
+        !isActive && "hover:bg-ternary"
       }`}
       onClick={onClick}
     >
