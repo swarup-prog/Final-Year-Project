7dd02bba8034f51f91780b3b851ca6a071049234
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-01 08:33:10 +0545

    Add icons to userRoutes

--- client/src/routes/userRoutes.jsx
+++ client/src/routes/userRoutes.jsx
@@ -1,24 +1,30 @@
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { MdPeople } from "react-icons/md";
+import { CgCommunity } from "react-icons/cg";
+
 const iconSize = 25;
 
-export default userRoutes = [
+const userRoutes = [
   {
-    path: "/app/feed",
+    path: "/app",
     name: "Feed",
     icon: "",
   },
   {
     path: "/app/discover",
     name: "Discover",
-    icon: "",
+    icon: <FaMagnifyingGlass />,
   },
   {
     path: "/app/friends",
     name: "Friends",
-    icon: "",
+    icon: <MdPeople />,
   },
   {
     path: "/app/communities",
     name: "Communities",
-    icon: "",
+    icon: <CgCommunity />,
   },
 ];
+
+export default userRoutes;
