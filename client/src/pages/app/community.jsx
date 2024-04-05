f0a4290cd000bb575b415cf751a4a67db6e4d549
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-05 12:57:06 +0545

    Add Community component

---
+++ client/src/pages/app/community.jsx
@@ -0,0 +1,21 @@
+const Community = () => {
+  return (
+    <div className="text-secondary px-10 py-10 overflow-y-auto">
+      <section>
+        <div className="text-xl text-accent font-semibold">
+          Your Communities
+        </div>
+      </section>
+      <div className="divider divider-accent"></div>
+      <section>
+        <div className="flex flex-col justify-center items-center gap-8 mt-20">
+          <span className="text-center">
+            You haven't joined any communities.
+          </span>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Community;
