be78f87bc1ddae0d25799b5ba1fee60505e97b60
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-05 12:56:55 +0545

    Refactor dashboard component

--- client/src/pages/admin/components/dashboard.jsx
+++ client/src/pages/admin/components/dashboard.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div>dashboard</div>;
+  return (
+    <div className="text-secondary px-10 py-10 overflow-y-auto">
+      <section>
+        <div className="text-xl text-accent font-semibold">Dashboard</div>
+      </section>
+      <div className="divider divider-accent"></div>
+    </div>
+  );
 };
 
 export default Dashboard;
