a19aa12e8f71732e989c23f9bf0511f2fca51862
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-05 12:57:52 +0545

    Add user-related API endpoints

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -3,13 +3,35 @@ const { User } = require("../models/user/User");
 const getUserInfo = async (req, res) => {
   try {
     const userId = req.params.id;
-    const user = await User.findById(userId, { password: 0, __v: 0 });
+    const user = await User.findById(userId, { password: 0, __v: 0 })
+      .populate("interestedGames")
+      .populate("buddies")
+      .populate("pendingRequest")
+      .populate("buddyRequest");
 
     if (!user) {
       return res.status(404).send({ message: "User not found." });
     }
 
     return res.status(200).json(user);
+  } catch (error) {
+    console.error("Error retrieving user:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find(
+      { role: { $ne: "admin" } },
+      { password: 0, __v: 0 }
+    )
+      .populate("interestedGames")
+      .populate("buddies")
+      .populate("pendingRequest")
+      .populate("buddyRequest");
+
+    return res.status(200).json(users);
   } catch (error) {
     console.error("Error retrieving user:", error);
     res.status(500).json({ error: "Internal server error" });
@@ -39,4 +61,31 @@ const updateInterestedGames = async (req, res) => {
   }
 };
 
-module.exports = { getUserInfo, updateInterestedGames };
+const sendBuddyRequest = async (req, res) => {
+  try {
+    const user = await User.findById(req.body.userId);
+    const addedUser = await User.findById(req.body.addedUserId);
+
+    if (!user || !addedUser) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    user.pendingRequest = [...user.pendingRequest, addedUser._id];
+    addedUser.buddyRequest = [...addedUser.buddyRequest, user._id];
+
+    await user.save();
+    await addedUser.save();
+
+    res.status(200).send({ message: "Friend request sent!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+};
+
+module.exports = {
+  getUserInfo,
+  updateInterestedGames,
+  getAllUsers,
+  sendBuddyRequest,
+};
