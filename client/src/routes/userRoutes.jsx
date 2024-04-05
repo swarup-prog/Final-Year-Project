95ecadfe2229e8926b0c43dc204706195cae158e
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-05 12:57:48 +0545

    Update userRoutes icons

--- client/src/routes/userRoutes.jsx
+++ client/src/routes/userRoutes.jsx
@@ -1,29 +1,48 @@
 import { FaMagnifyingGlass } from "react-icons/fa6";
-import { MdPeople } from "react-icons/md";
+import { TbUsers } from "react-icons/tb";
 import { CgCommunity } from "react-icons/cg";
+import { BiHomeAlt } from "react-icons/bi";
+import { FaRegUser } from "react-icons/fa";
+import { FaRegBell } from "react-icons/fa6";
+import { BsChatDots } from "react-icons/bs";
 
 const iconSize = 25;
 
 const userRoutes = [
   {
-    path: "/app",
-    name: "Feed",
-    icon: "",
+    path: "/app/home",
+    name: "Home",
+    icon: <BiHomeAlt size={iconSize} />,
   },
   {
     path: "/app/discover",
     name: "Discover",
-    icon: <FaMagnifyingGlass />,
+    icon: <FaMagnifyingGlass size={iconSize} />,
   },
   {
-    path: "/app/friends",
-    name: "Friends",
-    icon: <MdPeople />,
+    path: "/app/buddies",
+    name: "Buddies",
+    icon: <TbUsers size={iconSize} />,
+  },
+  {
+    path: "/app/messages",
+    name: "Messages",
+    icon: <BsChatDots size={iconSize} />,
+  },
+  {
+    path: "/app/notifications",
+    name: "Notifications",
+    icon: <FaRegBell size={iconSize} />,
+  },
+  {
+    path: "/app/profile",
+    name: "Profile",
+    icon: <FaRegUser size={iconSize} />,
   },
   {
     path: "/app/communities",
     name: "Communities",
-    icon: <CgCommunity />,
+    icon: <CgCommunity size={iconSize} />,
   },
 ];
 
