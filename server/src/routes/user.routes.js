60ad622a60933c043466baa8ebffb84432e0cb07
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-05 12:58:01 +0545

    Add new routes for getting all users and sending buddy requests

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -3,9 +3,14 @@ const router = require("express").Router();
 const {
   getUserInfo,
   updateInterestedGames,
+  getAllUsers,
+  sendBuddyRequest,
 } = require("../controllers/user.controller");
 
 router.route("/getUserInfo/:id").get(getUserInfo);
+router.route("/getAllUsers").get(getAllUsers);
 router.route("/updateInterestedgame").patch(updateInterestedGames);
+router.route("/updateInterestedgame").patch(updateInterestedGames);
+router.route("/sendBuddyRequest").patch(sendBuddyRequest);
 
 module.exports = router;
