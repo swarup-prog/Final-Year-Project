86ebd3eeb456100f5a126dc75b5ba592d53e521e
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-05 12:57:34 +0545

    Add Notifications component

---
+++ client/src/pages/app/components/notifications.jsx
@@ -0,0 +1,19 @@
+const Notifications = () => {
+  return (
+    <div className="text-secondary px-10 py-10 overflow-y-auto">
+      <section>
+        <div className="text-xl text-accent font-semibold">Notifications</div>
+      </section>
+      <div className="divider divider-accent"></div>
+      <section>
+        <div className="flex flex-col justify-center items-center gap-8 mt-20">
+          <span className="text-center">
+            You don't have any new notifications.
+          </span>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Notifications;
