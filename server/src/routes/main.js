5618cf896021ccd4b90709a6f9deb88622bdc972
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-08 13:28:12 +0545

    Add chat route and protect other routes with auth middleware

--- server/src/routes/main.js
+++ server/src/routes/main.js
@@ -2,12 +2,16 @@ const authRoute = require("./auth.routes.js");
 const userRoute = require("./user.routes.js");
 const gameRoute = require("./game.routes.js");
 const emailRoute = require("./emailVerification.routes.js");
+const chatRoute = require("./chat.routes.js");
+
+const { protect } = require("../middlewares/auth.middleware.js");
 
 const router = require("express").Router();
 
 router.use("/api/auth", authRoute);
-router.use("/api/user", userRoute);
-router.use("/api/game", gameRoute);
-router.use("/api/email", emailRoute);
+router.use("/api/user", protect, userRoute);
+router.use("/api/game", protect, gameRoute);
+router.use("/api/email", protect, emailRoute);
+router.use("/api/chat", protect, chatRoute);
 
 module.exports = router;
