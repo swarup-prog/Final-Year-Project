201d1d0f7e38b133b95c75e09e06dece9027ee07
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-08 13:27:06 +0545

    Add accessChat function to chat.controller.js

---
+++ server/src/controllers/chat.controller.js
@@ -0,0 +1,21 @@
+const Chat = require("../models/Chat");
+
+const accessChat = async (req, res) => {
+  const { userId } = req.body;
+
+  if (!userId) {
+    return res.status(400).json({ message: "User ID is required" });
+  }
+
+  let isChat = await Chat.find({
+    isGroupChat: false,
+    $and: [
+      { users: { $elemMatch: { $eq: req.user._id } } },
+      { users: { $elemMatch: { $eq: userId } } },
+    ],
+  })
+    .populate("users", "-password")
+    .populate("latestMessage");
+
+  is
+};
