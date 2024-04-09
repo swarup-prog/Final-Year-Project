ef62f722858d5c87a12931bfa73241e07daae34f
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-09 22:16:03 +0545

    Add addToGroup and removeFromGroup routes and functions to chat.routes.js and chat.controller.js

--- server/src/routes/chat.routes.js
+++ server/src/routes/chat.routes.js
@@ -3,6 +3,8 @@ const {
   fetchChats,
   createGroupChat,
   renameGroup,
+  addToGroup,
+  removeFromGroup,
 } = require("../controllers/chat.controller");
 
 const router = require("express").Router();
@@ -11,5 +13,7 @@ router.route("/").post(accessChat);
 router.route("/").get(fetchChats);
 router.route("/group-chat").post(createGroupChat);
 router.route("/rename-group").put(renameGroup);
+router.route("/add-user").put(addToGroup);
+router.route("/remove-user").put(removeFromGroup);
 
 module.exports = router;
