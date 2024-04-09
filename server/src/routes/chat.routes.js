a71da8ee58a6be211d288fbedea73951ff448e09
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-09 21:25:44 +0545

    Refactor chat.routes.js to include accessChat, fetchChats, and createGroupChat routes

--- server/src/routes/chat.routes.js
+++ server/src/routes/chat.routes.js
@@ -1,3 +1,13 @@
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+} = require("../controllers/chat.controller");
+
 const router = require("express").Router();
+
+router.route("/").post(accessChat);
+router.route("/").get(fetchChats);
+router.route("/group-chat").post(createGroupChat);
 
 module.exports = router;
