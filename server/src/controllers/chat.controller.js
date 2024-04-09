f5c8827ba3e64ec8ee508d7d968b24edd8bdc76b
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-09 22:15:54 +0545

    Add addToGroup and removeFromGroup functions to chat.controller.js

--- server/src/controllers/chat.controller.js
+++ server/src/controllers/chat.controller.js
@@ -128,4 +128,61 @@ const renameGroup = async (req, res) => {
   }
 };
 
-module.exports = { accessChat, fetchChats, createGroupChat, renameGroup };
+const removeFromGroup = async (req, res) => {
+  const { chatId, userId } = req.body;
+
+  // check if the requester is admin
+
+  const removed = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $pull: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!removed) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(removed);
+  }
+};
+
+const addToGroup = async (req, res) => {
+  const { chatId, userId } = req.body;
+
+  // check if the requester is admin
+
+  const added = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $push: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!added) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(added);
+  }
+};
+
+module.exports = {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  renameGroup,
+  addToGroup,
+  removeFromGroup,
+};
