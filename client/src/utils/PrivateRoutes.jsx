c273017e36ce042939ade40c57070236dfc8f092
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-09 18:38:30 +0545

    Refactor PrivateRoutes component to include role-based authentication logic

--- client/src/utils/PrivateRoutes.jsx
+++ client/src/utils/PrivateRoutes.jsx
@@ -1,9 +1,17 @@
 import { Outlet, Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import { getUserRole } from "../services";
 
-const PrivateRoutes = () => {
-  const isAuthenticated = localStorage.getItem("session-token") ? true : false;
+const PrivateRoutes = ({ role }) => {
+  const token = localStorage.getItem("session-token");
+  const userRole = getUserRole(token);
+  const isAuthenticated = token ? true : false;
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+  return isAuthenticated && role === userRole ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" />
+  );
 };
 
 export default PrivateRoutes;
