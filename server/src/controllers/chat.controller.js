87355c7511d7e5951301e7ce71487d31ac310e24
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-09 21:25:40 +0545

    Refactor chat.controller.js to include accessChat, fetchChats, and createGroupChat functions

--- server/src/controllers/chat.controller.js
+++ server/src/controllers/chat.controller.js
@@ -1,4 +1,5 @@
 const Chat = require("../models/Chat");
+const { User } = require("../models/User");
 
 const accessChat = async (req, res) => {
   const { userId } = req.body;
@@ -17,5 +18,88 @@ const accessChat = async (req, res) => {
     .populate("users", "-password")
     .populate("latestMessage");
 
-  is
+  isChat = await User.populate(isChat, {
+    path: "latestMessage.sender",
+    select: "name, username, profileImg",
+  });
+
+  if (isChat.length > 0) {
+    res.send(isChat[0]);
+  } else {
+    let chatData = {
+      chatName: "sender",
+      isGroupChat: false,
+      users: [req.user._id, userId],
+    };
+
+    try {
+      const createdChat = await Chat.create(chatData);
+      const FullChat = await Chat.findOne({ _id: createdChat._id }).populate(
+        "users",
+        "-password"
+      );
+      res.status(200).send(FullChat);
+    } catch (error) {
+      res.status(400);
+      throw new Error(error.message);
+    }
+  }
 };
+
+const fetchChats = async (req, res) => {
+  try {
+    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
+        results = await User.populate(results, {
+          path: "latestMessage.sender",
+          select: "name, username, profileImg",
+        });
+        res.status(200).send(results);
+      });
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+};
+
+const createGroupChat = async (req, res) => {
+  if (!req.body.users || !req.body.name) {
+    return res
+      .status(400)
+      .json({ message: "Chat name and users are required" });
+  }
+
+  let users = JSON.parse(req.body.users);
+
+  if (users.length < 2) {
+    return res
+      .status(400)
+      .json({ message: "Please add at least one more user" });
+  }
+  users.push(req.user);
+
+  let chatData = {
+    chatName: req.body.name,
+    isGroupChat: true,
+    users: users,
+    groupAdmin: req.user,
+  };
+
+  try {
+    const createdChat = await Chat.create(chatData);
+    const FullChat = await Chat.findOne({ _id: createdChat._id }).populate(
+      "users",
+      "-password"
+    );
+    res.status(200).send(FullChat);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+};
+
+module.exports = { accessChat, fetchChats, createGroupChat };
