381d2867f1f8f86fdb4de6aabfbcbffee3eb64fb
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-09 21:34:51 +0545

    Add renameGroup route to chat.routes.js

--- server/src/routes/chat.routes.js
+++ server/src/routes/chat.routes.js
@@ -2,6 +2,7 @@ const {
   accessChat,
   fetchChats,
   createGroupChat,
+  renameGroup,
 } = require("../controllers/chat.controller");
 
 const router = require("express").Router();
@@ -9,5 +10,6 @@ const router = require("express").Router();
 router.route("/").post(accessChat);
 router.route("/").get(fetchChats);
 router.route("/group-chat").post(createGroupChat);
+router.route("/rename-group").put(renameGroup);
 
 module.exports = router;
