64b8b48b627e9e42a460bc030bdd26737eeb79e8
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-15 22:28:53 +0545

    Refactor Chat component to handle group chats and display sender information

--- client/src/pages/app/components/chat.jsx
+++ client/src/pages/app/components/chat.jsx
@@ -1,14 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { toastError } from "../../../utils/toast";
-import axios from "axios";
+import { GroupChat, UserChat } from "../../../components";
+import { useSelector } from "react-redux";
+import { getSenderFull } from "../../../services/chatLogic";
 
 const Chat = () => {
-  const chatId = new URLSearchParams(window.location.search).get("chatId");
-  const isGroupChat = new URLSearchParams(window.location.search).get(
-    "isGroupChat"
-  );
+  const [isGroupChat, setIsGroupChat] = useState(false);
+  const [sender, setSender] = useState({});
 
-  return <div className="bg-primary"></div>;
+  const chat = useSelector((state) => state.chat.selectedChat);
+  const user = useSelector((state) => state.user.data);
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const isGroupChat = urlParams.get("isGroupChat");
+    if (isGroupChat === "true") {
+      setIsGroupChat(true);
+    } else {
+      setSender(getSenderFull(user?._id, chat?.users));
+    }
+  }, []);
+
+  return (
+    <div className="bg-primary w-full h-max ">
+      {/* Chat Heading  */}
+      <section className="border-b border-ternary chat-heading">
+        {isGroupChat ? (
+          <div>{chat.chatName.toUpperCase()}</div>
+        ) : (
+          <div>
+            <span>{JSON.stringify(sender)}</span>
+            {/* {getSenderFull(user?._id, chat?.users).toUpperCase()} */}
+          </div>
+        )}
+      </section>
+      {isGroupChat ? <GroupChat /> : <UserChat />}
+    </div>
+  );
 };
 
 export default Chat;
