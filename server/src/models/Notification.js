9b8048884973d8e1233ba54fda70d303fa1f1eb5
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-16 21:08:43 +0545

    Add Notification model to server/src/models/Notification.js

---
+++ server/src/models/Notification.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema(
+  {
+    message: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Notification = mongoose.model("Notification", notificationSchema);
+
+module.exports = { Notification };
