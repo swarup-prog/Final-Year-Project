7eeb9dbf4b6fbcc06bc901a3c51e3cc4a3306838
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-16 20:59:54 +0545

    Add socket.io event handlers for typing and stop typing events

--- server/src/index.js
+++ server/src/index.js
@@ -71,6 +71,9 @@ io.on("connection", (socket) => {
     console.log("User joined chat room: ", room);
   });
 
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
   socket.on("new message", (message) => {
     let chat = message.chat;
 
