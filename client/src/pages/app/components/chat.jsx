a2a11576a9ce849b786240c9246d89e9a84fe0a2
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-16 14:56:40 +0545

    Update Chat component to handle group chats and display sender information

--- client/src/pages/app/components/chat.jsx
+++ client/src/pages/app/components/chat.jsx
@@ -3,37 +3,64 @@ import { toastError } from "../../../utils/toast";
 import { GroupChat, UserChat } from "../../../components";
 import { useSelector } from "react-redux";
 import { getSenderFull } from "../../../services/chatLogic";
+import { Spinner } from "@chakra-ui/react";
 
 const Chat = () => {
   const [isGroupChat, setIsGroupChat] = useState(false);
-  const [sender, setSender] = useState({});
+  const [sender, setSender] = useState(null);
 
   const chat = useSelector((state) => state.chat.selectedChat);
   const user = useSelector((state) => state.user.data);
+
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
-    const isGroupChat = urlParams.get("isGroupChat");
-    if (isGroupChat === "true") {
-      setIsGroupChat(true);
-    } else {
-      setSender(getSenderFull(user?._id, chat?.users));
+    setIsGroupChat(urlParams.get("isGroupChat") === "true");
+
+    if (user && chat) {
+      if (!isGroupChat) {
+        const resolvedSender = getSenderFull(user._id, chat.users);
+        setSender(resolvedSender);
+      }
     }
-  }, []);
+  }, [user, chat, isGroupChat]);
+
+  if (!chat || (user && !isGroupChat && !sender)) {
+    return (
+      <Spinner size={"xl"} w={20} h={20} alignSelf={"center"} margin={"auto"} />
+    ); // Fallback or loading state
+  }
 
   return (
-    <div className="bg-primary w-full h-max ">
-      {/* Chat Heading  */}
-      <section className="border-b border-ternary chat-heading">
+    <div className="bg-primary w-full h-full  p-5 flex flex-col">
+      <section className=" chat-heading">
         {isGroupChat ? (
-          <div>{chat.chatName.toUpperCase()}</div>
+          <>
+            <div className="flex flex-col">
+              <div>{chat.chatName.toUpperCase()}</div>
+              <span className="font-normal text-sm">
+                {chat.users.length} Participants
+              </span>
+            </div>
+          </>
         ) : (
-          <div>
-            <span>{JSON.stringify(sender)}</span>
-            {/* {getSenderFull(user?._id, chat?.users).toUpperCase()} */}
-          </div>
+          sender && (
+            <div className="flex gap-5">
+              <img
+                src={sender.profileImg}
+                alt="profile"
+                className="w-10 h-10 rounded-full"
+              />
+              <div className="flex flex-col">
+                <div>{sender.name}</div>
+                <span className="font-normal text-sm">@{sender.username}</span>
+              </div>
+            </div>
+          )
         )}
       </section>
-      {isGroupChat ? <GroupChat /> : <UserChat />}
+      <div className="flex-grow">
+        {isGroupChat ? <GroupChat /> : <UserChat />}
+      </div>
     </div>
   );
 };
