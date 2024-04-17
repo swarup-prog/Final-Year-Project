f0b01fa44003c2d6558e9060706f83407eb20201
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-17 09:07:04 +0545

    Add cancelBuddyRequest route to user.routes.js

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -6,6 +6,7 @@ const {
   getAllUsers,
   sendBuddyRequest,
   getTotalUsers,
+  cancelBuddyRequest,
 } = require("../controllers/user.controller");
 
 router.route("/getUserInfo/:id").get(getUserInfo);
@@ -14,5 +15,6 @@ router.route("/getTotalUsers").get(getTotalUsers);
 router.route("/updateInterestedgame").patch(updateInterestedGames);
 router.route("/updateInterestedgame").patch(updateInterestedGames);
 router.route("/sendBuddyRequest").patch(sendBuddyRequest);
+router.route("/cancelBuddyRequest").patch(cancelBuddyRequest);
 
 module.exports = router;
