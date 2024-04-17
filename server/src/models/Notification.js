f04e1ae3cc7d6fa120da6affab24660d8cd1a830
Swarup Lamichhane <lswarup1234@gmail.com>
2024-04-17 09:07:33 +0545

    Refactor Notification model to use 'receiver' instead of 'user' field in server/src/models/Notification.js

--- server/src/models/Notification.js
+++ server/src/models/Notification.js
@@ -6,7 +6,7 @@ const notificationSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    user: {
+    receiver: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
